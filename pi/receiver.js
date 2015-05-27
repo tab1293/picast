7ca213e0447afb56e78738c96759eb32124bae4c
Tom Berger <tab1293@gmail.com>
2015-05-27 13:37:15 -0700

    Updated the pi receiver

--- pi/receiver.js
+++ pi/receiver.js
@@ -1,12 +1,21 @@
 var net = require('net');
 var dgram = require('dgram');
 var broadcastMsg = new Buffer("Where are you?");
-var client = dgram.createSocket("udp4");
+var broadcaster = dgram.createSocket("udp4");
 var desktopAddress = null;
 
+broadcaster.bind('4321');
+broadcaster.on('message', function() {
+    broadcaster.setBroadcast(true);
+    broadcaster.send(broadcastMsg, 0, broadcastMsg.length, 1234, "255.255.255.255");
+    console.log("Sending broadcast");
+});
+
+var relay = dgram.createSocket("udp4");
+var relayMsg = new Buffer("Send broadcast");
 var sendBroadcast = function() {
-    client.setBroadcast(true);
-    client.send(message, 0, message.length, 41234, "255.255.255.255");
+    relay.send(relayMsg, 0, relayMsg.length, 4321, "127.0.0.1");
+    console.log("Sending relay");
 };
 
 var timeout = setInterval(sendBroadcast, 1000);
@@ -16,11 +25,16 @@ var server = net.createServer(function(socket) {
     console.log("Client connected at " + address['address'] + " Port: " + address['port']);
     desktopAddress = address;
     clearInterval(timeout);
+    console.log("Interval cleared");
+
+    socket.on('close', function() {
+        console.log("Connection ended. Should send broadcasts again");
+        timeout = setInterval(sendBroadcast, 1000);
+    });
+
 });
 
 server.listen('1234', function() {
     console.log("Server is listening");
 });
-
-
 
